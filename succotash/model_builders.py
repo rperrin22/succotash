5b49882db2582ed73306e54c96d140b828c611c9
Rob Perrin <rperrin22@gmail.com>
2021-04-05 20:58:18 -0600

    fixed bug

--- succotash/model_builders.py
+++ succotash/model_builders.py
@@ -14,9 +14,9 @@ def build_dipping_plate(xmin,xmax,ymin,ymax,cellsize,xhinge,horizon_dip,z_base):
     ZZ[XX>=xhinge] = ZZ2[XX>=xhinge]
 
     # output the file
-    outputfilename = 'basement_surface_%1f_degrees.csv' % horizon_dip
+    outputfilename = 'basement_surface_%.1f_degrees.csv' % horizon_dip
     hdr = 'x-lims %d %d, y-lims %d %d, cellsize %d, xhinge %d, dip %d, z_base %d' % (xmin,xmax,ymin,ymax,cellsize,xhinge,horizon_dip,z_base)
-    np.savetxt(outputfilename,ZZ,delimiter=",",header=hdr,fmt='%.5f')
+    np.savetxt(outputfilename,ZZ,delimiter=",",header=hdr,fmt='%.3f')
 
 def build_effective_susc_uniform(xmin,xmax,ymin,ymax,cellsize,crust_susc,rem_inc,rem_dec,rem_susc,field_inc,field_dec):
 
@@ -47,6 +47,6 @@ def build_effective_susc_uniform(xmin,xmax,ymin,ymax,cellsize,crust_susc,rem_inc
     ZZ = np.ones(XX.shape) * plotting_model * conversion_factor
 
     # output the file
-    outputfilename = 'basement_susceptibility_%d.csv' % rem_susc
+    outputfilename = 'basement_susceptibility_%.1f.csv' % rem_susc
     hdr = 'x-lims %d %d, y-lims %d %d, cellsize %d, crust-susceptibility %.2f, field-inc %.2f, field-dec %.2f, rem-susceptibility %.2f, rem-inc %.2f, rem-dec %.2f' % (xmin,xmax,ymin,ymax,cellsize,crust_susc,field_inc,field_dec,rem_susc,rem_inc,rem_dec)
-    np.savetxt(outputfilename,ZZ,delimiter=",",header=hdr,fmt='%.5f')
+    np.savetxt(outputfilename,ZZ,delimiter=",",header=hdr,fmt='%.3f')
