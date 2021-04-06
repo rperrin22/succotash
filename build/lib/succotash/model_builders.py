01b66089f3dc816d604d62d8bbaa0b45eca51964
Rob Perrin <rperrin22@gmail.com>
2021-04-05 19:32:21 -0600

    first package made

---
+++ build/lib/succotash/model_builders.py
@@ -0,0 +1,19 @@
+def build_dipping_plate(xmin,xmax,ymin,ymax,cellsize,xhinge,horizon_dip,z_base):
+    # some nifty description about this function
+
+    import numpy as np 
+    
+    # create x-y mesh
+    xvec = np.linspace(xmin,xmax,int((xmax+dxdy-xmin)/dxdy))
+    yvec = np.linspace(ymin,ymax,int((ymax+dxdy-ymin)/dxdy))
+    XX,YY = np.meshgrid(xvec,yvec)
+
+    # build z-surface
+    ZZ = np.ones(XX.shape) * z_base
+    ZZ2 = z_base - (XX-xhinge)*np.tan(np.deg2rad(horizon_dip))
+    ZZ[XX>=xhinge] = ZZ2[XX>=xhinge]
+
+    # output the file
+    outputfilename = 'basement_surface_%d_degrees.csv' % horizon_dip
+    hdr = 'x-lims %d %d, y-lims %d %d, cellsize %d, xhinge %d, dip %d, z_base %d' % (xmin,xmax,ymin,ymax,cellsize,xhinge,horizon_dip,z_base)
+    np.savetxt(outputfilename,ZZ,delimiter=",",header=hdr)
--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-from setuptools import setup
-setup(name='ubiquitous-succotash',
-version='0.1',
-description='Initial build of the package',
-url='https://github.com/rperrin22/ubiquitous-succotash',
-author='Rob Perrin',
-author_email='robert.perrin@ucalgary.ca',
-license='MIT',
-packages=['ubiquitous-succotash'],
-zip_safe=False)
+from setuptools import setup, find_packages
+setup(name='succotash',
+    version='0.1',
+    packages=find_packages(),
+)
